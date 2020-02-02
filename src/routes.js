b9fea2d1d5084f1064e4e1da282b34649601cad7
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-02-02 20:46:35 -0200

    adiciona conexao com o banco e cadastro de usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'vai carai' });
+routes.get('/cadastro', async (req, res) => {
+  const user = await User.create({
+    name: 'Lucas Nogueira',
+    email: 'lucas_snogueira@hotmail.com',
+    password_hash: 'lala1234',
+  });
+
+  return res.json(user);
 });
 
 module.exports = routes;
