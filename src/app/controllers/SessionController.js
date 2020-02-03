80ad35458f6f7a93751277ba30f7f4834ea33b11
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-02-03 08:24:33 -0200

    wip jwt auth

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+    return res.json({ user: {{ id, name, email } }}, token: jwt.sign({id}, '', {expiresIn: '7d'}));
+  }
+}
+
+export default new SessionController();
