4520fcda619c35e73762c73bd98b1d96b3826e05
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-03-04 00:12:41 -0300

    adiciona upload de imagem na aplicacao

--- src/config/multer.js
+++ src/config/multer.js
@@ -9,7 +9,8 @@ export default {
     filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, res) => {
         if (err) return cb(err);
-        return cb(res.toString('hex') + extname(file.originalname));
+
+        return cb(null, res.toString('hex') + extname(file.originalname));
       });
     },
   }),
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import multerConfig from './config/multer';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
@@ -13,5 +16,8 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ teste: 'vai carai' });
+});
 
 module.exports = routes;
