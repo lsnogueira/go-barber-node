85c8df7a88284ab74a8eb9ce515bcdc10ba7a4c4
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-03-31 23:07:17 -0300

    configura nodemailer e envia email para barbeiro quando ha cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6b3a3e6f744511',
+    pass: '8ba6bf775b9d12',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com.br>',
+  },
+};
